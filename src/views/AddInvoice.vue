8dc60854f343c1eee42750d51c0c986703ec5e68
rrd <rrd@1108.cc>
2019-12-11 18:30:17 +0100

    new inputs added

--- src/views/AddInvoice.vue
+++ src/views/AddInvoice.vue
@@ -16,6 +16,20 @@
                 </select>
             </div>
         </div>
+
+        <div class="row">
+            <div class="column small-12 large-6">
+                <label for="partner-id">{{$t("partner")}}</label>
+                <input type="text" v-model.lazy="partner_id" list="partners" id="partner-id" name="partner_id" autocomplete="off">
+                <datalist id="partners">
+                    <option v-for="partner in partners" :key="partner.id" :data-value="partner.id">{{ partner.name }}</option>
+                </datalist>
+            </div>
+            <div class="column small-12 large-6">
+                <label for="date">{{$t("date")}}</label>
+                <input type="date" v-model="date">
+            </div>
+        </div>
     </div>
 </template>
 
@@ -31,6 +45,9 @@ export default {
             storage_id: 0,
             invoicetypes: {},
             invoicetype_id: 0,
+            partners: {},
+            partner_id: '',
+            date: (new Date()).toISOString().split('T')[0],
         }
     },
 
@@ -45,6 +62,12 @@ export default {
         axios.get(process.env.VUE_APP_API_URL + 'invoicetypes.json?company=' + this.$store.state.company.id + '&ApiKey=' + this.$store.state.user.api_token)
             .then(response => {
                 this.invoicetypes = response.data
+            })
+            .catch(err => console.error(err))
+
+        axios.get(process.env.VUE_APP_API_URL + 'partners.json?company=' + this.$store.state.company.id + '&ApiKey=' + this.$store.state.user.api_token)
+            .then(response => {
+                this.partners = response.data
             })
             .catch(err => console.error(err))
     },
