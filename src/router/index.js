af87f94065714156cdd422ea406bfdd9c104377d
rrd <rrd@1108.cc>
2019-12-21 17:43:29 +0100

    about removed

--- src/router/index.js
+++ src/router/index.js
@@ -44,14 +44,6 @@ const routes = [
         name: 'stock',
         component: () => import('../views/Stock.vue')
       },
-      {// TODO remove this
-        path: 'about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-      },
     ]
   }
 ]
