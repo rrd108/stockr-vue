a04cf768358a76938d9d22de671604479bf85ceb
rrd <rrd@1108.cc>
2019-12-26 15:44:03 +0100

    fix stock

--- src/components/FilteredProductTbody.vue
+++ src/components/FilteredProductTbody.vue
@@ -2,8 +2,8 @@
     <tbody>
         <tr v-for="product in filteredItems" :key="product.id" v-show="!product.hidden">
             <td><a :href="'view/' + product.id">{{product.name}}</a></td>
-            <td>{{code}}</td>
-            <td>{{size}}</td>
+            <td>{{product.code}}</td>
+            <td>{{product.size}}</td>
             <td class="text-right">{{product.stock | toNum(0)}}</td>
             <td class="text-right">{{product.avaragePurchasePrice | toCurrency}}</td>
             <td class="text-right">{{product.lastPurchasePrice | toCurrency}}</td>
