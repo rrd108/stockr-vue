39318ad1a00ca6aa9c65ced7467ddc76064533bf
rrd <rrd@1108.cc>
2020-09-21 16:51:24 +0200

    indentation fixed to 2 spaces

--- src/components/FilteredProductTbody.vue
+++ src/components/FilteredProductTbody.vue
@@ -1,46 +1,46 @@
 <template>
-    <tbody>
-        <tr v-for="product in filteredItems" :key="product.id">
-            <td>
+  <tbody>
+    <tr v-for="product in filteredItems" :key="product.id">
+      <td>
         <i class="fi-pencil" @click="edit(product.id)"></i>
-                <router-link :to="'products/' + product.id">
-                    <i class="fi-foot"> {{product.name}}</i>
-                </router-link>
-            </td>
-            <td>{{product.code}}</td>
-            <td>{{product.size}}</td>
-            <td class="text-right">{{product.stock | toNum(1)}}</td>
-            <td class="text-right">{{product.sells | toNum(1)}}</td>
-            <td class="text-right">{{product.avaragePurchasePrice | toCurrency}}</td>
-            <td class="text-right">{{product.lastPurchasePrice | toCurrency}}</td>
-            <td class="text-right">{{product.stock * product.avaragePurchasePrice | toCurrency}}</td>
-            <td class="text-right">{{product.stock * product.lastPurchasePrice | toCurrency}}</td>
-        </tr>
-    </tbody>
+        <router-link :to="'products/' + product.id">
+            <i class="fi-foot"> {{product.name}}</i>
+        </router-link>
+      </td>
+      <td>{{product.code}}</td>
+      <td>{{product.size}}</td>
+      <td class="text-right">{{product.stock | toNum(1)}}</td>
+      <td class="text-right">{{product.sells | toNum(1)}}</td>
+      <td class="text-right">{{product.avaragePurchasePrice | toCurrency}}</td>
+      <td class="text-right">{{product.lastPurchasePrice | toCurrency}}</td>
+      <td class="text-right">{{product.stock * product.avaragePurchasePrice | toCurrency}}</td>
+      <td class="text-right">{{product.stock * product.lastPurchasePrice | toCurrency}}</td>
+    </tr>
+  </tbody>
 </template>
 
 <script>
 import RowFilterMixin from '@/mixins/RowFilterMixin'
 
 export default {
-    name: 'FilteredTbody',
+  name: 'FilteredTbody',
 
-    props: {
-        products: {
-            type: Array,
-            required: true
-        },
+  props: {
+    products: {
+      type: Array,
+      required: true
     },
+  },
 
-    data() {
-        return {
-            model: 'products'
-        }
-    },
+  data() {
+    return {
+      model: 'products'
+    }
+  },
 
-    mixins: [
-        RowFilterMixin
-    ],
+  mixins: [
+    RowFilterMixin
+  ],
 
   methods: {
     edit(id) {
