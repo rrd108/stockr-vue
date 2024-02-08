f9521d29a23431567058dd8cfc9648bc80017ee5
rrd108 <rrd@webmania.cc>
2024-02-08 18:35:41 +0100

    basic view works

--- src/pages/ViewInvoice.vue
+++ src/pages/ViewInvoice.vue
@@ -1,159 +1,39 @@
-<template>
-  <div v-if="isLoaded" class="small-12 columns content">
-    <h3>
-      {{ invoice.storage.company.name }}
-      <i class="fi-trash" @click="deleteInvoice" v-show="invoice.status != 'd'"></i>
-    </h3>
-
-    <table class="vertical-table" :class="{ deleted: invoice.status == 'd' }">
-      <tbody>
-        <tr>
-          <th scope="row">Partner</th>
-          <td>{{ invoice.partner.name }}</td>
-          <th scope="row">Raktár</th>
-          <td>{{ invoice.storage.name }}</td>
-        </tr>
-        <tr>
-          <th scope="row">Bizonylat típus</th>
-          <td class="pointer">
-            <i class="fi-pencil" v-show="!onEdit" @click="changeInvoicetype"> {{ invoice.invoicetype.name }}</i>
-            <select v-model="invoicetype_id" v-show="onEdit" @change="changeInvoicetype">
-              <option v-for="invoicetype in invoicetypes" :key="invoicetype.id" :value="invoicetype.id">
-                {{ invoicetype.name }}
-              </option>
-            </select>
-          </td>
-          <th scope="row">Dátum</th>
-          <td>{{ toLocaleDateString(invoice.date) }}</td>
-        </tr>
-        <tr>
-          <th scope="row">Szám</th>
-          <td>
-            <span v-html="$options.filters.invoiceNumber(invoice.number)"></span>
-            <i
-              v-if="invoice.partner.group.id != 4 && invoice.number.indexOf('|') == -1"
-              @click="onInvoicing = true"
-              class="fi-page-export-pdf pointer"
-            >
-              Billingo</i
-            >
-          </td>
-          <th scope="row">Típus</th>
-          <td>{{ invoice.sale ? 'Eladás' : 'Beszerzés' }}</td>
-        </tr>
-        <tr>
-          <th scope="row">Valuta</th>
-          <td>{{ invoice.currency }}</td>
-          <th scope="row">PDF</th>
-          <td>
-            <i @click="getPdf" class="fi-page-export-pdf pointer"> PDF</i>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-
-    <table cellpadding="0" cellspacing="0" :class="{ deleted: invoice.status == 'd' }">
-      <thead>
-        <tr :class="invoice.sale ? 'out' : 'in'">
-          <th class="text-center" scope="col">Termék</th>
-          <th class="text-center" scope="col">Mennyiség</th>
-          <th class="text-center" scope="col">Ár</th>
-          <th class="text-center" scope="col">Összeg</th>
-          <th class="text-center" scope="col">ÁFA</th>
-          <th class="text-center" scope="col">ÁFA</th>
-          <th class="text-center" scope="col">Bruttó</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in invoice.items" :key="item.id">
-          <td>
-            {{ item.product.name }} {{ item.product.size }} {{ item.product.code }} <i>{{ item.product.en_name }}</i>
-          </td>
-          <td class="text-right">{{ item.quantity }}</td>
-          <td class="text-right">{{ toCurrency(item.price, currency) }}</td>
-          <td class="text-right">
-            {{ toCurrency(item.price * item.quantity, currency) }}
-          </td>
-          <td class="text-right">{{ item.product.vat }} %</td>
-          <td class="text-right">
-            {{ toCurrency((item.product.vat * item.price * item.quantity) / 100, currency) }}
-          </td>
-          <td class="text-right">
-            {{ toCurrency(item.price * item.quantity * (1 + item.product.vat / 100), currency) }}
-          </td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td>Összesen</td>
-          <td class="text-right">
-            {{
-              toNum(
-                invoice.items.reduce((total, item) => total + parseInt(item.quantity), 0),
-                1
-              )
-            }}
-          </td>
-          <td></td>
-          <td class="text-right">
-            {{
-              toCurrency(
-                invoice.items.reduce((total, item) => total + item.price * item.quantity, 0),
-                currency
-              )
-            }}
-          </td>
-          <td></td>
-          <td class="text-right">
-            {{
-              toCurrency(
-                invoice.items.reduce((total, item) => total + (item.price * item.quantity * item.product.vat) / 100, 0),
-                currency
-              )
-            }}
-          </td>
-          <td class="text-right">
-            {{
-              toCurrency(
-                invoice.items.reduce((total, item) => total + item.price * item.quantity * (1 + item.product.vat / 100), 0),
-                currency
-              )
-            }}
-          </td>
-        </tr>
-      </tfoot>
-    </table>
-
-    <aside v-show="onInvoicing">
-      <h2>Billingo számla kiállítás <span @click="onInvoicing = false">x</span></h2>
-      <dl>
-        <dt>Teljesítés dátuma</dt>
-        <dd><input type="date" v-model="fulfillment_date" /></dd>
-        <dt>Fizetési határidő</dt>
-        <dd><input type="date" v-model="due_date" /></dd>
-        <dt>Fizetési mód</dt>
-        <dd>
-          <input type="radio" id="kp" value="1" v-model="payment_method" />
-          <label for="kp">Készpénz</label>
-          <br />
-          <input type="radio" id="transfer" value="2" v-model="payment_method" />
-          <label for="transfer">Átutalás</label>
-        </dd>
-        <dt>Megjegyzés a számlára</dt>
-        <dd><input type="text" v-model="invoice_comment" /></dd>
-      </dl>
-      <button class="button" @click="generateInvoice">Számlázás</button>
-    </aside>
-  </div>
-</template>
-
-<script>
+<script setup>
+  import { ref } from 'vue'
   import axios from 'axios'
+  import { useRoute } from 'vue-router'
+  import { useStockrStore } from '@/stores'
+
   import toCurrency from '@/composables/useToCurrency'
   import toNum from '@/composables/useToNum'
   import toLocaleDateString from '@/composables/useToLocaleDateString'
+  import invoiceNumber from '@/composables/useInvoiceNumber'
 
-  export default {
+  const store = useStockrStore()
+  const route = useRoute()
+
+  const invoice = ref({})
+
+  const storedInvoice = store.invoices.find(invoice => invoice.id == route.params.id)
+  if (storedInvoice) {
+    const storage = store.storages.find(storage => storage.id == storedInvoice.storage_id)
+    invoice.value = {
+      ...storedInvoice,
+      storage: {
+        ...storage,
+        company: store.company,
+      },
+      partner: store.partners.find(partner => partner.id == storedInvoice.partner_id),
+      items: [
+        ...storedInvoice.items.map(item => ({
+          ...item,
+          product: store.products.find(product => product.id == item.product_id),
+        })),
+      ],
+    }
+  }
+
+  /*  export default {
     name: 'ViewInvoce',
 
     data() {
@@ -311,8 +191,157 @@
           .catch(error => console.log(error))
       },
     },
-  }
+  }*/
 </script>
+
+<template>
+  <div v-if="invoice.id" class="small-12 columns content">
+    <h3>
+      {{ invoice.storage.company.name }}
+      <i class="fi-trash" @click="deleteInvoice" v-show="invoice.status != 'd'"></i>
+    </h3>
+
+    <table class="vertical-table" :class="{ deleted: invoice.status == 'd' }">
+      <tbody>
+        <tr>
+          <th scope="row">Partner</th>
+          <td>{{ invoice.partner.name }}</td>
+          <th scope="row">Raktár</th>
+          <td>{{ invoice.storage.name }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Bizonylat típus</th>
+          <td class="pointer">
+            <i class="fi-pencil" v-show="!onEdit" @click="changeInvoicetype"> {{ invoice.invoicetype.name }}</i>
+            <select v-model="invoicetype_id" v-show="onEdit" @change="changeInvoicetype">
+              <option v-for="invoicetype in invoicetypes" :key="invoicetype.id" :value="invoicetype.id">
+                {{ invoicetype.name }}
+              </option>
+            </select>
+          </td>
+          <th scope="row">Dátum</th>
+          <td>{{ toLocaleDateString(invoice.date) }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Szám</th>
+          <td>
+            <span v-html="invoiceNumber(invoice.number)"></span>
+            <i
+              v-if="invoice.partner.group.id != 4 && invoice.number.indexOf('|') == -1"
+              @click="onInvoicing = true"
+              class="fi-page-export-pdf pointer"
+            >
+              Billingo</i
+            >
+          </td>
+          <th scope="row">Típus</th>
+          <td>{{ invoice.sale ? 'Eladás' : 'Beszerzés' }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Valuta</th>
+          <td>{{ invoice.currency }}</td>
+          <th scope="row">PDF</th>
+          <td>
+            <i @click="getPdf" class="fi-page-export-pdf pointer"> PDF</i>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <table cellpadding="0" cellspacing="0" :class="{ deleted: invoice.status == 'd' }">
+      <thead>
+        <tr :class="invoice.sale ? 'out' : 'in'">
+          <th class="text-center" scope="col">Termék</th>
+          <th class="text-center" scope="col">Mennyiség</th>
+          <th class="text-center" scope="col">Ár</th>
+          <th class="text-center" scope="col">Összeg</th>
+          <th class="text-center" scope="col">ÁFA</th>
+          <th class="text-center" scope="col">ÁFA</th>
+          <th class="text-center" scope="col">Bruttó</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in invoice.items" :key="item.id">
+          <td>
+            {{ item.product.name }} {{ item.product.size }} {{ item.product.code }} <i>{{ item.product.en_name }}</i>
+          </td>
+          <td class="text-right">{{ item.quantity }}</td>
+          <td class="text-right">{{ toCurrency(item.price, currency) }}</td>
+          <td class="text-right">
+            {{ toCurrency(item.price * item.quantity, currency) }}
+          </td>
+          <td class="text-right">{{ item.product.vat }} %</td>
+          <td class="text-right">
+            {{ toCurrency((item.product.vat * item.price * item.quantity) / 100, currency) }}
+          </td>
+          <td class="text-right">
+            {{ toCurrency(item.price * item.quantity * (1 + item.product.vat / 100), currency) }}
+          </td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Összesen</td>
+          <td class="text-right">
+            {{
+              toNum(
+                invoice.items.reduce((total, item) => total + parseInt(item.quantity), 0),
+                1
+              )
+            }}
+          </td>
+          <td></td>
+          <td class="text-right">
+            {{
+              toCurrency(
+                invoice.items.reduce((total, item) => total + item.price * item.quantity, 0),
+                currency
+              )
+            }}
+          </td>
+          <td></td>
+          <td class="text-right">
+            {{
+              toCurrency(
+                invoice.items.reduce((total, item) => total + (item.price * item.quantity * item.product.vat) / 100, 0),
+                currency
+              )
+            }}
+          </td>
+          <td class="text-right">
+            {{
+              toCurrency(
+                invoice.items.reduce((total, item) => total + item.price * item.quantity * (1 + item.product.vat / 100), 0),
+                currency
+              )
+            }}
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+
+    <aside v-show="onInvoicing">
+      <h2>Billingo számla kiállítás <span @click="onInvoicing = false">x</span></h2>
+      <dl>
+        <dt>Teljesítés dátuma</dt>
+        <dd><input type="date" v-model="fulfillment_date" /></dd>
+        <dt>Fizetési határidő</dt>
+        <dd><input type="date" v-model="due_date" /></dd>
+        <dt>Fizetési mód</dt>
+        <dd>
+          <input type="radio" id="kp" value="1" v-model="payment_method" />
+          <label for="kp">Készpénz</label>
+          <br />
+          <input type="radio" id="transfer" value="2" v-model="payment_method" />
+          <label for="transfer">Átutalás</label>
+        </dd>
+        <dt>Megjegyzés a számlára</dt>
+        <dd><input type="text" v-model="invoice_comment" /></dd>
+      </dl>
+      <button class="button" @click="generateInvoice">Számlázás</button>
+    </aside>
+  </div>
+</template>
 
 <style scoped>
   h3 {
