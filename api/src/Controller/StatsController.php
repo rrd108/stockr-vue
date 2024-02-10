bd53fb62ffcf32ca1e6fb55861eb06ef776e0bf3
rrd108 <rrd@webmania.cc>
2024-02-10 15:01:30 +0100

    fix whitespace

--- api/src/Controller/StatsController.php
+++ api/src/Controller/StatsController.php
@@ -1,13 +1,15 @@
 <?php
+
 namespace App\Controller;
 
+use Cake\Core\Configure;
 use App\Controller\AppController;
 use Cake\ORM\Locator\LocatorAwareTrait;
-use Cake\Core\Configure;
 
 class StatsController extends AppController
 {
     use LocatorAwareTrait;
+
     public function index()
     {
         $invoicesTable = $this->fetchTable('Invoices');
@@ -18,23 +20,25 @@ class StatsController extends AppController
             'sells' => collection(
                 $invoicesTable->find('withTotal')
                     ->where([
-                        'sale' => true, 'Storages.company_id' => Configure::read('company_id'),
+                        'sale' => true,
+                        'Storages.company_id' => Configure::read('company_id'),
                         'YEAR(Invoices.date)' => date('Y')
-                        ])
-                )->sumOf('total'),
+                    ])
+            )->sumOf('total'),
             'purchases' => collection(
                 $invoicesTable->find('withTotal')
                     ->where([
-                        'sale' => false, 'Storages.company_id' => Configure::read('company_id'),
+                        'sale' => false,
+                        'Storages.company_id' => Configure::read('company_id'),
                         'YEAR(Invoices.date)' => date('Y')
-                        ])
-                )->sumOf('total'),
+                    ])
+            )->sumOf('total'),
             'stock' => $productsTable->find('stock')->sumOf('stock'),
         ];
         $invoices = $invoicesTable->find()->where([
             'Storages.company_id' => Configure::read('company_id'),
             'YEAR(Invoices.date)' => date('Y')
-            ])->count();
+        ])->count();
         $partners = $partnersTable->find()->count();
         $products = $productsTable->find()->count();
 
